89a138ef75e31aab0aebf85213fc59fd70fb0eb4
Abolfazl-Khodayari <khodayari.ut@gmail.com>
2022-06-04 18:45:33 +0430

    first Upload

--- chess.cpp
+++ chess.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "Hello World! Im sina and this is myfirst github change";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
